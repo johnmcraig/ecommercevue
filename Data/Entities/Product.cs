1be2e0b1a44a533f1dd2b9e9ab3763c72b5f1eba
johnmcraig <jmcraig32884@gmail.com>
2019-05-26 04:50:28 -0400

    Created Product class with propeties

---
+++ Data/Entities/Product.cs
@@ -0,0 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace ecommercevue.Data.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Slug { get; set; }
+        [Required]
+        public string Thumbnail { get; set; }
+        [Required]
+        public string ShortDescription { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public decimal Price { get; set; }
+    }
+}
