1b0841a4431abd555a9ffb4d0627f4904cac5649
johnmcraig <jmcraig32884@gmail.com>
2019-05-26 03:02:21 -0400

    Created error page for wildcard route

---
+++ ClientApp/components/error-pages/NotFound.vue
@@ -0,0 +1,15 @@
+<template>
+    <p>
+        404 Not Found. Sorry :(
+    </p>
+</template>
+
+<script>
+export default {
+    name: 'not-found'
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
