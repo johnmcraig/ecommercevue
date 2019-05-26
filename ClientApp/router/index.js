f199b46fdb7c1d28ec3cb5b53fa440900c23c89a
johnmcraig <jmcraig32884@gmail.com>
2019-05-26 03:03:28 -0400

    Imported error page and setup wildcard route

--- ClientApp/router/index.js
+++ ClientApp/router/index.js
@@ -2,21 +2,14 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '../components/Home'
 import UserList from '../components/UserList'
+import NotFound from '../components/error-pages/NotFound'
 
 Vue.use(Router)
 
 const routes = [
-    { 
-      path: '/',
-      name: 'Home', 
-      component: Home
-    },
-    {
-      path: '/Users',
-      name: 'Users',
-      component: UserList
-
-    }
+    { path: '/', name: 'Home', component: Home },
+    { path: '/Users', name: 'Users', component: UserList },
+    { path: '*', name: 'Error', component: NotFound }
   ]
 
 export default new Router({
