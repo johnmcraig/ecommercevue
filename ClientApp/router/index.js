12da50ed202fc48185479c6e03a57ab03c657440
johnmcraig <jmcraig32884@gmail.com>
2019-05-26 04:53:05 -0400

    Added more routes and imported components

--- ClientApp/router/index.js
+++ ClientApp/router/index.js
@@ -3,12 +3,17 @@ import Router from 'vue-router'
 import Home from '../components/Home'
 import UserList from '../components/UserList'
 import NotFound from '../components/error-pages/NotFound'
+import Catalogue from '../pages/Catalogue.vue'
+import Product from '../pages/Product.vue'
+
 
 Vue.use(Router)
 
 const routes = [
     { path: '/', name: 'Home', component: Home },
-    { path: '/Users', name: 'Users', component: UserList },
+    { path: '/products', name: 'Catalogue', component: Catalogue },
+    { path: '/products/:slug', name: 'Product', component: Product },
+    { path: '/users', name: 'Users', component: UserList },
     { path: '*', name: 'Error', component: NotFound }
   ]
 
